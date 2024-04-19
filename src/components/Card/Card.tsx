52880a7862c1b5f0cc8be1ad845f8d6dfd666ecb
Luis Martinez <luismartinezwebdev@gmail.com>
2024-04-19 11:07:55 +0700

    shiny card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,14 +1,17 @@
 import { cn } from '@/utils/cn';
+import Styles from './Card.module.css';
 
 const Card = ({
   children,
-  className
+  className,
+  shiny,
 }: {
   children?: React.ReactNode;
   className?: string;
+  shiny?: boolean;
 }): React.JSX.Element => {
   return (
-    <div className={cn("p-5 rounded-lg shadow-lg bg-transparent border border-primary-500 text-primary-900", className)}>
+    <div className={cn("p-5 rounded-lg shadow-lg bg-transparent border border-primary-500 text-primary-900", className, shiny && Styles.shiny)}>
       {children}
     </div>
   );
