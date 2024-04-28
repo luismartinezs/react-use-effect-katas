865b261fd51186c0f9d47777e4c9116aa14d4dd8
Luis Martinez <luismartinezwebdev@gmail.com>
2024-04-28 23:31:59 +0700

    cool hover card

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/Card";
 function App() {
   return (
     <div className="flex items-center justify-center p-4">
-      <Card className="max-w-3xl" shiny>
+      <Card className="max-w-3xl" shiny hoverEffect>
         <h1 className="text-3xl font-bold">React useEffect Katas</h1>
         <p className="mt-4">
           Welcome to the React useEffect Katas repository! This project is
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,13 +5,15 @@ const Card = ({
   children,
   className,
   shiny,
+  hoverEffect
 }: {
   children?: React.ReactNode;
   className?: string;
   shiny?: boolean;
+  hoverEffect?: boolean;
 }): React.JSX.Element => {
   return (
-    <div className={cn("p-5 rounded-lg shadow-primary-500/20 shadow-lg bg-transparent border border-primary-500 text-primary-900", className, shiny && Styles.shiny)}>
+    <div className={cn("p-5 rounded-lg shadow-primary-500/20 shadow-lg bg-transparent border border-primary-500 text-primary-900", className, shiny && Styles.shiny, hoverEffect && Styles.hover)}>
       {children}
     </div>
   );
